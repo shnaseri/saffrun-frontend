394a92b56b2fed940fe2e9dc8b763036e52c57f9
ali alamdari <ali.alamdari9088@gmail.com>
2021-11-08 16:19:39 +0330

    changePass

--- saffrun/src/views/UserInfo/Account.js
+++ saffrun/src/views/UserInfo/Account.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React from "react";
 import {
   Media,
   Row,
@@ -14,15 +14,14 @@ import {
   Dropdown,
   DropdownItem,
   DropdownMenu,
-  DropdownToggle
-} from "reactstrap"
-import { Trash2 , Edit} from "react-feather"
-import userImg from "../../assets/img/profile/Generic-profile-picture.jpg.webp"
-import Checkbox from "../../components/@vuexy/checkbox/CheckboxesVuexy"
-import { Check, Lock } from "react-feather"
-import axios from "axios"
+  DropdownToggle,
+} from "reactstrap";
+import { Trash2, Edit } from "react-feather";
+import userImg from "../../assets/img/profile/Generic-profile-picture.jpg.webp";
+import Checkbox from "../../components/@vuexy/checkbox/CheckboxesVuexy";
+import { Check, Lock } from "react-feather";
+import axios from "axios";
 // import { Avatar } from 'react-native-elements';
-
 
 class UserAccountTab extends React.Component {
   constructor(props) {
@@ -30,34 +29,39 @@ class UserAccountTab extends React.Component {
 
     this.toggle = this.toggle.bind(this);
     this.state = {
-      dropdownOpen: false
+      dropdownOpen: false,
     };
   }
-
+  state = {
+    currPass: "",
+    newPass: "",
+    repPass: "",
+  };
   toggle() {
-    this.setState(prevState => ({
-      dropdownOpen: !prevState.dropdownOpen
+    this.setState((prevState) => ({
+      dropdownOpen: !prevState.dropdownOpen,
     }));
   }
-  hiddenFileInput = React.createRef()
-  handleClick = event => {
-    console.log(this.hiddenFileInput.current) ;
-    this.hiddenFileInput.current.click()
+  hiddenFileInput = React.createRef();
+  handleClick = (event) => {
+    console.log(this.hiddenFileInput.current);
+    this.hiddenFileInput.current.click();
   };
-  handleChange = event => {
+  handleChange = (event) => {
     const fileUploaded = event.target.files[0];
-    this.props.updateImg((URL.createObjectURL(fileUploaded)));
+    this.props.updateImg(URL.createObjectURL(fileUploaded));
     console.log(URL.createObjectURL(fileUploaded));
   };
-  handleDel = ()=>{
+  handleDel = () => {
     this.props.delImg();
-  }
-  handleAvatar = () =>{
-    if (this.props.userData["avatar"] != "") return this.props.userData["avatar"]
-    else{
-      return userImg
+  };
+  handleAvatar = () => {
+    if (this.props.userData["avatar"] != "")
+      return this.props.userData["avatar"];
+    else {
+      return userImg;
     }
-  }
+  };
   render() {
     return (
       <Row>
@@ -72,42 +76,43 @@ class UserAccountTab extends React.Component {
                 alt="user profile image"
                 height="84"
                 width="84"
-                
               />
-             
-              <Dropdown direction="left"  isOpen={this.state.dropdownOpen} toggle={this.toggle} style={{marginTop:-15 }}>
-                <DropdownToggle  style={{padding:0,color:"#ff9f43" , float:"left"}} caret>
-                <Edit style={{right:0  }}  /> 
+
+              <Dropdown
+                direction="left"
+                isOpen={this.state.dropdownOpen}
+                toggle={this.toggle}
+                style={{ marginTop: -15 }}
+              >
+                <DropdownToggle
+                  style={{ padding: 0, color: "#ff9f43", float: "left" }}
+                  caret
+                >
+                  <Edit style={{ right: 0 }} />
                 </DropdownToggle>
-                <DropdownMenu style={{position:"relative"}}>
-                  <DropdownItem onClick={this.handleClick} >تغییر</DropdownItem>
+                <DropdownMenu style={{ position: "relative" }}>
+                  <DropdownItem onClick={this.handleClick}>تغییر</DropdownItem>
                   <DropdownItem divider />
-                  <DropdownItem onClick={this.props.delImg} >حذف</DropdownItem>
+                  <DropdownItem onClick={this.props.delImg}>حذف</DropdownItem>
                 </DropdownMenu>
               </Dropdown>
-              {/* <Avatar   showEditButton/> */}
             </Media>
             <Media className="mt-2" body>
               <Media className="font-medium-1 text-bold-600" tag="p" heading>
-              {this.props.userData["fname"]}{this.props.userData["lname"]}
+                {this.props.userData["fname"]}
+                {this.props.userData["lname"]}
               </Media>
-              {/* <div className="d-flex flex-wrap">
-                <Button onClick={this.handleClick} className="mr-1" color="primary" outline>
-                  تغییر
-                </Button> */}
-                <input
+              <input
                 type="file"
                 ref={this.hiddenFileInput}
                 onChange={this.handleChange}
-                style={{display: 'none'}} 
+                style={{ display: "none" }}
               />
-                {/* <Button  className="mr-1" color="danger" outline >حذف تصویر</Button> */}
-              {/* </div> */}
             </Media>
           </Media>
         </Col>
         <Col sm="12">
-          <Form onSubmit={e => e.preventDefault()}>
+          <Form onSubmit={(e) => e.preventDefault()}>
             <Row>
               <Col md="6" sm="12">
                 <FormGroup>
@@ -124,8 +129,13 @@ class UserAccountTab extends React.Component {
               <Col md="6" sm="12">
                 <FormGroup>
                   <Label for="password">رمز عبور</Label>
-                  <Input type="password" value = {this.props.userData["password"]} name="password" id="password" onChange={this.props.updateData}>
-                  </Input>
+                  <Input
+                    type="password"
+                    value={this.props.userData["password"]}
+                    name="password"
+                    id="password"
+                    onChange={this.props.updateData}
+                  ></Input>
                 </FormGroup>
               </Col>
 
@@ -144,8 +154,14 @@ class UserAccountTab extends React.Component {
               <Col md="6" sm="12">
                 <FormGroup>
                   <Label for="lName">نام خانوادگی</Label>
-                  <Input type="text" value={this.props.userData["lname"]} name="lName" id="lname" placeholder="lname" onChange={this.props.updateData}>
-                  </Input>
+                  <Input
+                    type="text"
+                    value={this.props.userData["lname"]}
+                    name="lName"
+                    id="lname"
+                    placeholder="lname"
+                    onChange={this.props.updateData}
+                  ></Input>
                 </FormGroup>
               </Col>
               <Col md="6" sm="12">
@@ -172,7 +188,77 @@ class UserAccountTab extends React.Component {
                   />
                 </FormGroup>
               </Col>
-              <Col sm="12" style={{marginTop:35}}>
+              <Col sm="12" style={{ marginTop: 35 }}>
+                <div className="permissions border px-2">
+                  <div className="title pt-2 pb-0">
+                    <Lock size={19} />
+                    <span className="text-bold-500 font-medium-2 ml-50">
+                      تغییر رمز کاربری
+                    </span>
+                    <hr />
+                    <Col md="6" sm="12">
+                      <FormGroup>
+                        <Label for="password">رمز قبلی</Label>
+                        <Input
+                          type="password"
+                          value={this.state.currPass}
+                          id="password"
+                          placeholder="رمز قبلی"
+                          onChange={(e) => this.setState({ currPass: e.target.value })}
+                        />
+                      </FormGroup>
+                    </Col>
+                    <Col md="6" sm="12">
+                      <FormGroup>
+                        <Label for="newpassword">رمز جدید</Label>
+                        <Input
+                          type="text"
+                          value={this.state.newPass}
+                          name="newpassword"
+                          id="newpassword"
+                          placeholder="رمز جدید"
+                          onChange={(e) => this.setState({ newPass: e.target.value })}
+                        ></Input>
+                      </FormGroup>
+                    </Col>
+
+                    <Col md="6" sm="12">
+                      <FormGroup>
+                        <Label for="RepPass">تکرار رمز جدید</Label>
+                        <Input
+                          type="text"
+                          value={this.state.repPass}
+                          id="RepPass"
+                          placeholder="تکرار رمز"
+                          invalid={this.state.repPass !== this.state.newPass}
+                          onChange={(e) => this.setState({ repPass: e.target.value })}
+                        />
+                        {this.state.repPass !== this.state.newPass && (
+                          <small style={{ color: "red", fontSize: "11px" }}>
+                            رمز شما مطابقت ندارد
+                          </small>
+                        )}
+                      </FormGroup>
+                    </Col>
+                    <Col
+                      className="d-flex justify-content-end flex-wrap mt-2"
+                      sm="12"
+                      style={{ marginTop: 50 }}
+                    >
+                      <Button
+                        className="mr-1"
+                        color="primary"
+                        // onClick={this.props.postData}
+                        style={{ margin: 20 }}
+                      >
+                        تغییر رمز
+                      </Button>
+                      {/* <Button color="flat-warning">بازگرداندن به قبل</Button> */}
+                    </Col>
+                  </div>
+                </div>
+              </Col>
+              <Col sm="12" style={{ marginTop: 35 }}>
                 <div className="permissions border px-2">
                   <div className="title pt-2 pb-0">
                     <Lock size={19} />
@@ -307,18 +393,22 @@ class UserAccountTab extends React.Component {
               <Col
                 className="d-flex justify-content-end flex-wrap mt-2"
                 sm="12"
-                style={{marginTop:50}}
+                style={{ marginTop: 50 }}
               >
-                <Button className="mr-1" color="primary" onClick={this.props.postData} style={{marginTop:20}}>
+                <Button
+                  className="mr-1"
+                  color="primary"
+                  onClick={this.props.postData}
+                  style={{ marginTop: 20 }}
+                >
                   اعمال تغییرات
                 </Button>
-                {/* <Button color="flat-warning">بازگرداندن به قبل</Button> */}
               </Col>
             </Row>
           </Form>
         </Col>
       </Row>
-    )
+    );
   }
 }
-export default UserAccountTab
+export default UserAccountTab;
--- saffrun/src/views/UserInfo/UserInfo.js
+++ saffrun/src/views/UserInfo/UserInfo.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React from "react";
 import {
   Card,
   CardBody,
@@ -8,72 +8,77 @@ import {
   NavItem,
   NavLink,
   TabContent,
-  TabPane
-} from "reactstrap"
-import classnames from "classnames"
-import { User, Info, Users } from "react-feather"
-import AccountTab from "./Account"
-import InfoTab from "./Information"
-import SocialTab from "./Social"
-import "../../assets/scss/pages/users.scss"
-import axios from "axios"
-import { toast } from "react-toastify"
+  TabPane,
+} from "reactstrap";
+import classnames from "classnames";
+import { User, Info, Users } from "react-feather";
+import AccountTab from "./Account";
+import InfoTab from "./Information";
+import SocialTab from "./Social";
+import "../../assets/scss/pages/users.scss";
+import axios from "axios";
+import { toast } from "react-toastify";
+import ComponentSpinner from "../../components/@vuexy/spinner/Loading-spinner";
 
 class UserEdit extends React.Component {
   state = {
     activeTab: "1",
-    userData : {}
-  }
-  notifySuccess = () => toast.success("اطلاعات به روزرسانی شد", {
-    position: toast.POSITION.TOP_CENTER
-  })
-  notifyError = () => toast.error("خطا",{
-    position: toast.POSITION.TOP_CENTER
-  })
-  componentDidMount = async  ()=>{
-    console.log("asd")
-    let userData = await axios.get("https://6176598703178d00173daba2.mockapi.io/users/20");
-    this.setState({userData : userData.data})
-    console.log(this.state.userData)
-  }
-  postData = async ()=>{
-    let x =await axios.put("https://6176598703178d00173daba2.mockapi.io/users/6",this.state.userData);
-    if (x.status === 200)
-    {
-      this.notifySuccess()
-    }
-    else{
-      this.notifyError()
-    }
-    
-  }
-  updateImg= (url) =>{
-    let userData = {...this.state.userData ,"avatar": url};
-    this.setState({ userData })
-  }
-  delImg = ()=>{
-    let userData = {...this.state.userData ,"avatar": ""};
-    this.setState({ userData })
-    console.log(this.state.userData)
-  }
-  updateData = (e) =>{
-    let userData = {...this.state.userData ,[e.target.id]: e.target.value};
-
-    this.setState({ userData })
-    console.log(this.state.userData)
+    userData: {},
+    loadSpinner: true,
   };
-    
-  
-  toggle = tab => {
+  notifySuccess = () =>
+    toast.success("اطلاعات به روزرسانی شد", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+  notifyError = () =>
+    toast.error("خطا", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+  componentDidMount = async () => {
+    console.log("asd");
+    let userData = await axios.get(
+      "https://6176598703178d00173daba2.mockapi.io/users/20"
+    );
+    this.setState({ userData: userData.data, loadSpinner: false });
+    // console.log(this.state.userData)
+  };
+  postData = async () => {
+    let x = await axios.put(
+      "https://6176598703178d00173daba2.mockapi.io/users/6",
+      this.state.userData
+    );
+    if (x.status === 200) {
+      this.notifySuccess();
+    } else {
+      this.notifyError();
+    }
+  };
+  updateImg = (url) => {
+    let userData = { ...this.state.userData, avatar: url };
+    this.setState({ userData });
+  };
+  delImg = () => {
+    let userData = { ...this.state.userData, avatar: "" };
+    this.setState({ userData });
+    console.log(this.state.userData);
+  };
+  updateData = (e) => {
+    let userData = { ...this.state.userData, [e.target.id]: e.target.value };
+
+    this.setState({ userData });
+    console.log(this.state.userData);
+  };
+
+  toggle = (tab) => {
     this.setState({
-      activeTab: tab
-    })
-  }
+      activeTab: tab,
+    });
+  };
   render() {
-    return (
-      
+    return this.state.loadSpinner ? (
+      <ComponentSpinner />
+    ) : (
       <Row>
-        
         <Col sm="12">
           <Card>
             <CardBody className="pt-2">
@@ -81,10 +86,10 @@ class UserEdit extends React.Component {
                 <NavItem>
                   <NavLink
                     className={classnames({
-                      active: this.state.activeTab === "1"
+                      active: this.state.activeTab === "1",
                     })}
                     onClick={() => {
-                      this.toggle("1")
+                      this.toggle("1");
                     }}
                   >
                     <User size={16} />
@@ -94,23 +99,23 @@ class UserEdit extends React.Component {
                 <NavItem>
                   <NavLink
                     className={classnames({
-                      active: this.state.activeTab === "2"
+                      active: this.state.activeTab === "2",
                     })}
                     onClick={() => {
-                      this.toggle("2")
+                      this.toggle("2");
                     }}
                   >
-                    <Info size={16}  />
+                    <Info size={16} />
                     <span className="align-middle ml-50">اطلاعات شخصی</span>
                   </NavLink>
                 </NavItem>
                 <NavItem>
                   <NavLink
                     className={classnames({
-                      active: this.state.activeTab === "3"
+                      active: this.state.activeTab === "3",
                     })}
                     onClick={() => {
-                      this.toggle("3")
+                      this.toggle("3");
                     }}
                   >
                     <Users size={16} />
@@ -120,20 +125,29 @@ class UserEdit extends React.Component {
               </Nav>
               <TabContent activeTab={this.state.activeTab}>
                 <TabPane tabId="1">
-                  <AccountTab userData={this.state.userData} postData = {this.postData} updateData = {this.updateData} updateImg = {this.updateImg} delImg = {this.delImg} />
+                  <AccountTab
+                    userData={this.state.userData}
+                    postData={this.postData}
+                    updateData={this.updateData}
+                    updateImg={this.updateImg}
+                    delImg={this.delImg}
+                  />
                 </TabPane>
                 <TabPane tabId="2">
-                  <InfoTab updateData = {this.updateData} userData ={this.state.userData}/>
+                  <InfoTab
+                    updateData={this.updateData}
+                    userData={this.state.userData}
+                  />
                 </TabPane>
                 <TabPane tabId="3">
-                  <SocialTab userData = {this.state.userData} />
+                  <SocialTab userData={this.state.userData} />
                 </TabPane>
               </TabContent>
             </CardBody>
           </Card>
         </Col>
       </Row>
-    )
+    );
   }
 }
-export default UserEdit
+export default UserEdit;
