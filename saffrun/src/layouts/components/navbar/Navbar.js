8d9bc3690f9a3acb561eb2ad5f89014b23969cc2
amirhossein <amirsmvt9@gmail.com>
2021-12-29 18:44:12 +0330

    (Fix):navbar

--- saffrun/src/layouts/components/navbar/Navbar.js
+++ saffrun/src/layouts/components/navbar/Navbar.js
@@ -1,128 +1,117 @@
-import React from "react"
-import { Navbar } from "reactstrap"
-// import { connect } from "react-redux"
-import classnames from "classnames"
-// import { useAuth0 } from "../../../authServices/auth0/auth0Service"
-// import {
-//   logoutWithJWT,
-//   logoutWithFirebase
-// } from "../../../redux/actions/auth/loginActions"
-import NavbarBookmarks from "./NavbarBookmarks"
-import NavbarUser from "./NavbarUser"
-import userImg from "../../../assets/img/portrait/small/avatar-s-11.jpg"
+import React from "react";
+import { Navbar } from "reactstrap";
+import classnames from "classnames";
+import NavbarBookmarks from "./NavbarBookmarks";
+import NavbarUser from "./NavbarUser";
+import userImg from "../../../assets/img/portrait/small/avatar-s-11.jpg";
+import axios from "axios";
+import urlDomain from "../../../utility/urlDomain";
+import { history } from "../../../history";
+import isAuthenticated from "../../../utility/authenticated";
+import defaultImg from "../../../assets/img/profile/Generic-profile-picture.jpg.webp";
 
-const UserName = props => {
-  // let username = ""
-  // if (props.userdata !== undefined) {
-  //   username = props.userdata.name
-  // } else if (props.user.login.values !== undefined) {
-  //   username = props.user.login.values.name
-  //   if (
-  //     props.user.login.values.loggedInWith !== undefined &&
-  //     props.user.login.values.loggedInWith === "jwt"
-  //   ) {
-  //     username = props.user.login.values.loggedInUser.name
-  //   }
-  // } else {
-  //   username = "John Doe"
-  // }
+const UserName=props=>
+{
 
-  return "mmd"
+  return props.userdata;
 }
-const ThemeNavbar = props => {
-  // const { user } = useAuth0()
-  const colorsArr = [ "primary", "danger", "success", "info", "warning", "dark"]
-  const navbarTypes = ["floating" , "static" , "sticky" , "hidden"]
-  return (
-    <React.Fragment>
-      <div className="content-overlay" />
-      <div className="header-navbar-shadow" />
-      <Navbar
-        className={classnames(
-          "header-navbar navbar-expand-lg navbar navbar-with-menu navbar-shadow",
-          {
-            "navbar-light": props.navbarColor === "default" || !colorsArr.includes(props.navbarColor),
-            "navbar-dark": colorsArr.includes(props.navbarColor),
-            "bg-primary":
-              props.navbarColor === "primary" && props.navbarType !== "static",
-            "bg-danger":
-              props.navbarColor === "danger" && props.navbarType !== "static",
-            "bg-success":
-              props.navbarColor === "success" && props.navbarType !== "static",
-            "bg-info":
-              props.navbarColor === "info" && props.navbarType !== "static",
-            "bg-warning":
-              props.navbarColor === "warning" && props.navbarType !== "static",
-            "bg-dark":
-              props.navbarColor === "dark" && props.navbarType !== "static",
-            "d-none": props.navbarType === "hidden" && !props.horizontal,
-            "floating-nav":
-              (props.navbarType === "floating" && !props.horizontal) || (!navbarTypes.includes(props.navbarType) && !props.horizontal),
-            "navbar-static-top":
-              props.navbarType === "static" && !props.horizontal,
-            "fixed-top": props.navbarType === "sticky" || props.horizontal,
-            "scrolling": props.horizontal && props.scrolling
 
-          }
-        )}
-      >
-        <div className="navbar-wrapper">
-          <div className="navbar-container content">
-            <div
-              className="navbar-collapse d-flex justify-content-between align-items-center"
-              id="navbar-mobile"
-            >
-              <div className="bookmark-wrapper">
-                <NavbarBookmarks
-                  sidebarVisibility={props.sidebarVisibility}
-                  handleAppOverlay={props.handleAppOverlay}
+const colorsArr = ["primary", "danger", "success", "info", "warning", "dark"];
+const navbarTypes = ["floating", "static", "sticky", "hidden"];
+class ThemeNavbar extends React.Component {
+  state = {
+    username:"",
+    image:"",
+  };
+  imgGenerator = (x) => {
+    return x
+      ? `http://localhost:8000${x.thumbnail}`
+      : defaultImg;
+  };
+  async componentDidMount ()
+  {
+    let authenticated = await isAuthenticated();
+    if (!authenticated) history.push("/login");
+    let token = localStorage.getItem("access");
+    token = `Bearer ${token}`;
+    try {
+      let response = await axios.get(`${urlDomain}/profile/user/`, {
+        headers: { Authorization: token },
+      });
+      this.setState({ username: response.data.username,image:response.data.avatar["image"]});
+    } catch (e) {
+      // this.setState({ loadSpinner: false });
+    }
+  }
+  render() {
+    let { props } = this;
+    return (
+      <React.Fragment>
+        <div className="content-overlay" />
+        <div className="header-navbar-shadow" />
+        <Navbar
+          className={classnames(
+            "header-navbar navbar-expand-lg navbar navbar-with-menu navbar-shadow",
+            {
+              "navbar-light":
+                props.navbarColor === "default" ||
+                !colorsArr.includes(props.navbarColor),
+              "navbar-dark": colorsArr.includes(props.navbarColor),
+              "bg-primary":
+                props.navbarColor === "primary" &&
+                props.navbarType !== "static",
+              "bg-danger":
+                props.navbarColor === "danger" && props.navbarType !== "static",
+              "bg-success":
+                props.navbarColor === "success" &&
+                props.navbarType !== "static",
+              "bg-info":
+                props.navbarColor === "info" && props.navbarType !== "static",
+              "bg-warning":
+                props.navbarColor === "warning" &&
+                props.navbarType !== "static",
+              "bg-dark":
+                props.navbarColor === "dark" && props.navbarType !== "static",
+              "d-none": props.navbarType === "hidden" && !props.horizontal,
+              "floating-nav":
+                (props.navbarType === "floating" && !props.horizontal) ||
+                (!navbarTypes.includes(props.navbarType) && !props.horizontal),
+              "navbar-static-top":
+                props.navbarType === "static" && !props.horizontal,
+              "fixed-top": props.navbarType === "sticky" || props.horizontal,
+              scrolling: props.horizontal && props.scrolling,
+            }
+          )}
+        >
+          <div className="navbar-wrapper">
+            <div className="navbar-container content">
+              <div
+                className="navbar-collapse d-flex justify-content-between align-items-center"
+                id="navbar-mobile"
+              >
+                <div className="bookmark-wrapper">
+                  <NavbarBookmarks
+                    sidebarVisibility={props.sidebarVisibility}
+                    handleAppOverlay={props.handleAppOverlay}
+                  />
+                </div>
+                {props.horizontal ? (
+                  <div className="logo d-flex align-items-center">
+                    <div className="brand-logo mr-50"></div>
+                    <h2 className="text-primary brand-text mb-0">Vuexy</h2>
+                  </div>
+                ) : null}
+                <NavbarUser
+                  userName={<UserName userdata={this.state.username} />}
+                  userImg={this.imgGenerator(this.state.image)}
+                  
                 />
               </div>
-              {props.horizontal ? (
-                <div className="logo d-flex align-items-center">
-                  <div className="brand-logo mr-50"></div>
-                  <h2 className="text-primary brand-text mb-0">Vuexy</h2>
-                </div>
-              ) : null}
-              <NavbarUser
-                handleAppOverlay={props.handleAppOverlay}
-                changeCurrentLang={props.changeCurrentLang}
-                userName={<UserName userdata={UserName} />}
-                userImg={
-                  // props.user.login.values !== undefined &&
-                  // props.user.login.values.loggedInWith !== "jwt" &&
-                  // props.user.login.values.photoUrl
-                  //   ? props.user.login.values.photoUrl 
-                  //   : user !== undefined && user.picture ? user.picture
-                     userImg
-                }
-                // loggedInWith={
-                //   props.user !== undefined &&
-                //   props.user.login.values !== undefined
-                //     ? props.user.login.values.loggedInWith
-                //     : null
-                // }
-                // logoutWithJWT={props.logoutWithJWT}
-                // logoutWithFirebase={props.logoutWithFirebase}
-              />
             </div>
           </div>
-        </div>
-      </Navbar>
-    </React.Fragment>
-  )
+        </Navbar>
+      </React.Fragment>
+    );
+  }
 }
-
-// const mapStateToProps = state => {
-//   return {
-//     user: state.auth
-//   }
-// }
-
-export default 
-// connect(mapStateToProps, {
-  // logoutWithJWT,
-  // logoutWithFirebase,
-  // useAuth0
-// })
-(ThemeNavbar)
+export default ThemeNavbar;
